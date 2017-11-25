cbe6fca0217edbea0d965d7bcaf30deb9c9ef419
Amanda Worthington <amanda@amandaworthington.com>
2017-11-25 12:09:45 -0500

    copy services to other pages and update links

---
+++ code/js/main.js
@@ -0,0 +1,8 @@
+var $grid = $('.grid').imagesLoaded( function() {
+  // init Masonry after all images have loaded
+  $grid.masonry({
+    // options...
+    itemSelector: '.grid-item',
+    columnWidth: 200
+  });
+});
